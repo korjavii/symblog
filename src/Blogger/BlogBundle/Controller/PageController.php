79e3350f9433d51a64d1a9a43295a970635cdd14
Korjavii <korjavii.dyu@gmail.com>
2016-08-07 20:03:39 +0300

    annotations used; about page added

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -3,11 +3,31 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class PageController extends Controller
 {
+    /**
+     * @Route(
+     *      "/",
+     *      methods="GET",
+     *      name="index_page"
+     * )
+     */
     public function indexAction()
     {
         return $this->render('BloggerBlogBundle:Page:index.html.twig');
+    }
+    
+    /**
+     * @Route(
+     *      "/about",
+     *      methods="GET",
+     *      name="about_page"
+     * )
+     */
+    public function aboutAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
 }
