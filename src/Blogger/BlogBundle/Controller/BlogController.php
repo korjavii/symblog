ba0373c75af5027053da0c029c7a2b917ed8e8aa
Korjavii <korjavii.dyu@gmail.com>
2016-08-28 00:43:29 +0300

    show blog page

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,40 @@
+<?php
+// src/Blogger/BlogBundle/Controller/BlogController.php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Blog controller.
+ *
+ * @Route("/blog")
+ */
+class BlogController extends Controller
+{
+    /**
+     * @Route(
+     *      "/{id}",
+     *      name="blog_show",
+     *      requirements={"id": "\d+"}
+     * )
+     * @Method({"GET"})
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
+}
