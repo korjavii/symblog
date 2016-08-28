73c7933e3bb2cc82c0adedc3d166f4bb4822a03b
Korjavii <korjavii.dyu@gmail.com>
2016-08-28 10:48:00 +0300

    show latest blog rows

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -21,7 +21,15 @@ class PageController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+        $em = $this->getDoctrine()
+                   ->getManager();
+
+        $blogs = $em->getRepository('BloggerBlogBundle:Blog')
+                    ->getLatestBlogs();
+
+        return $this->render('BloggerBlogBundle:Page:index.html.twig', array(
+            'blogs' => $blogs
+        ));
     }
     
     /**
